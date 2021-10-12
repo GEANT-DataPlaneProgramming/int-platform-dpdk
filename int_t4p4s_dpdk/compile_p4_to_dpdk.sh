d242cfdcda60d6399aa9135cfd0787ba927771d6
Mário Kuka <kuka@cesnet.cz>
2021-10-12 15:12:11 +0000

    INT_T4P4s - compile_p4_to_dpdk.sh: fix selecting p4 standard

--- int_t4p4s_dpdk/compile_p4_to_dpdk.sh
+++ int_t4p4s_dpdk/compile_p4_to_dpdk.sh
@@ -1,5 +1,5 @@
 P4SRC="../p4src/int_v1.0/int.p4"
-PARAMS="arch=dpdk hugepages=2048 model=v1model smem cores=2 ports=2x2"
+PARAMS="arch=dpdk hugepages=2048 std=p4-16 model=v1model smem cores=2 ports=2x2"
 
 cd t4p4s
 . ./t4p4s_envvars.sh
