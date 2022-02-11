eac0051ad854ba79952576236043d290ba8f9d77
Mário Kuka <mariokukalv@gmail.com>
2022-02-11 11:11:46 +0000

    freeze dpdk version to 20.11.3

--- int_t4p4s_dpdk/run_dpdk_app.sh
+++ int_t4p4s_dpdk/run_dpdk_app.sh
@@ -1,5 +1,6 @@
-INTERFACES="--vdev=net_tap0,iface=foo0 --vdev=net_tap1,iface=foo1"
+INTERFACES="--vdev=net_tap0,iface=foo0 --vdev=net_tap1,iface=foo1 --no-pci"
+#INTERFACES="-w 0000:06:00.0 --vdev=net_tap1,iface=foo1"
 DPDK_APP="./t4p4s/build/int@std/build/int"
 
 sudo ./dpdk_hugepages_setup.sh -c 1024
-sudo $DPDK_APP -c 0x3 -n 4 $INTERFACES -- -p 0x3 --config "(0,0,0),(0,1,1),(1,0,0),(1,1,1)"
+sudo $DPDK_APP -c 0x1 -n 4 $INTERFACES -- -p 0x3 --config "(0,0,0),(1,0,0)"
--- int_t4p4s_dpdk/t4p4s_bootstrap.sh
+++ int_t4p4s_dpdk/t4p4s_bootstrap.sh
@@ -2,4 +2,5 @@ sudo apt update
 sudo apt install -y clang cmake meson gdb net-tools
 git clone --recursive https://github.com/GEANT-DataPlaneProgramming/t4p4s.git
 cd t4p4s
-REPO_PATH_p4c=https://github.com/GEANT-DataPlaneProgramming/p4c INSTALL_STAGE5_GRPC=no PARALLEL_INSTALL=no . ./bootstrap-t4p4s.sh
+DPDK_VSN=20.11
+DPDK_VSN=20.11 DPDK_VERSION=20.11 DPDK_FILEVSN=20.11.3 REPO_PATH_p4c=https://github.com/GEANT-DataPlaneProgramming/p4c INSTALL_STAGE5_GRPC=no PARALLEL_INSTALL=no . ./bootstrap-t4p4s.sh
